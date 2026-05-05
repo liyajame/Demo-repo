e9d99fee0a10727eff58ded83c6f5fce637c6deb
liyajame <liyajames04@gmail.com>
2026-05-05 17:00:39 -0400

    Update Program.cs

--- movieratingapp/movieratingapp/Program.cs
+++ movieratingapp/movieratingapp/Program.cs
@@ -6,17 +6,6 @@ using System.Net.Http.Headers;
 
 namespace Themovieratingapp;
 
-// Review Comments for Code:
-//  1) Watchlist -> Add Movie should insert into the main movies storage and not maintain its own separate and independent lists
-//  2) Disallow duplicate movies in watchlist
-//  3) Handle dependency of Watchlists on Movies without accessing the disk every time you add a movie to a watchlist
-//  4) Menus and prompts should be indicative of what they are going to do.
-//  5) In cases where specific options are available (i.e Add Movie -> Select Type (Movie/Series)) consider prompting user to type a number such as 1 for Movie, 2 for Series - partially done
-//  6) Remove any unused code, namespaces, etc
-//  7) Follow code standards for spacing, static vs sealed vs etc
-//  8) Make sure your menus and prompts feel consistent to the user
-//  9) Ensuring punctuation and or spelling for sentences.
-
 static class Program
 {
     private static List<Movie> Movies;
